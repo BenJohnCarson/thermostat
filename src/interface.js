1d3084bee51918710bdf17fdff136728e059652e
Sam Jones <sam.jones494@googlemail.com>
2017-02-03 11:36:53 +0000

    configured weather api

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
 	var thermostat = new Thermostat();
 	updateTemp()
 
+ 	$('.select-city').submit(function(event) {
+  		event.preventDefault();
+  		var city = $('.current-city').val();
+  		$.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=28da720ac77959beb32c9a1a92ebf7d7&units=metric', function(data) {
+    		$('.outsidetemp').text(data.main.temp);
+  		})
+	})
+
+
 	$('.tempup').click(function () {
 		thermostat.up();
 		updateTemp()
